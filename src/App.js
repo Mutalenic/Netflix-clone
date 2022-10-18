22118dca58135dc158337dc7073e79579e577085
mutalenic <nicomutale@gmail.com>
2022-10-18 11:06:18 +0200

    Add navbar import navbar in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      <h>
-        NetFlix
-      </h>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
