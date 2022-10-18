7d9c12846450db999f8d9f8b2eed5d0efe1372f5
mutalenic <nicomutale@gmail.com>
2022-10-18 12:29:32 +0200

    Add api and api endpoints to requests

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '754452fd9b1b87b6839e903c0162a00c'
+
+const movieRequests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}  &language=en-US&page=1`,
+    requestLatest:`https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestUpCOming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestSimilar: `https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=${key}&language=en-US&page=1`
+};
+
+export default movieRequests;
