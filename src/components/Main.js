b506739176bf5220893f486d3de18f4d0b82884b
mutalenic <nicomutale@gmail.com>
2022-10-18 12:50:25 +0200

    Add main to home page fetch data from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,16 @@
-import React from 'react';
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import movieRequests from '../Requests';
 
 function Main() {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(movieRequests.requestLatest).then((response) => {
+      setMovies(response.data);
+    });
+  }, []);
+  console.log(movies);
   return <div>Main</div>;
 }
 
