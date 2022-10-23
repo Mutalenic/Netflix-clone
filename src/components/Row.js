d86065fc4363b068d462b515cb2637b0d1aff00a
mutalenic <nicomutale@gmail.com>
2022-10-23 17:57:37 +0200

    Update images on row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -15,7 +15,13 @@ function Row({ title, fetchURL }) {
     <>
       <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
       <div className="relative flex items-center">
-        <div id={'slider'}></div>
+        <div id={'slider'}>
+          {movies.map((item, id) => (
+            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+              <img src={`https://image.tmdb.org/t/p/w-500/${item?.backdrop_path}`} alt="item?.title" />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
